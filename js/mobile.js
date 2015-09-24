8df49bc6310f6ece71b9afa7b1aaa19415f579af
Daniel Llinas <danllinas@gmail.com>
2015-09-23 23:45:54 -0400

    About halfway done.

--- js/mobile.js
+++ js/mobile.js
@@ -2,4 +2,7 @@ $(document).ready(function()    {
   $('img#burger').click(function() {
     $('.drop-down').toggle();
   });
+  $('.hamburger li').click(function(){
+    $('.drop-down').toggle();
+  });
 });
